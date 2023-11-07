19d687956cd8a8d56bd6d8ab1eb9bc556c133b3b
David Mejia <jose.david.mejia.meneses@gmail.com>
2023-11-07 15:06:44 -0500

    tercer commit

--- src/co/iniciandojava/Funciones.java
+++ src/co/iniciandojava/Funciones.java
@@ -4,5 +4,18 @@ public class Funciones {
 
     public static void main(String[] args) {
 
+
+        //opción 1:
+        showMenu();
+
+
+        //opción 2:
+    }
+
+     static void showMenu(){
+         System.out.println("Bienvenidos al E-comerce de zapatillas");
+         System.out.println("1 - Ver zapatillas");
+         System.out.println("2 - Comprar zapatillas");
+         System.out.println("3 - Salir");
     }
 }
